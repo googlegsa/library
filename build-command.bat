cdf9d918a694cb1bdbe014072dd8f4ecb08eeeda
P.J. Opalinski <pjo@google.com>
2011-06-28 15:25:33 -0700

    Merge commit.

--- build-command.bat
+++ build-command.bat
@@ -1,5 +1,5 @@
 mkdir -p classes
-javac -d classes src/adaptorlib/*java src/adaptorlib/tests/*java src/templateadaptor/*java
+javac -d classes src/adaptorlib/*java src/adaptorlib/tests/*java src/adaptortemplate/*java
 copy src/logging.properties classes/
 
 REM TODO: Reintroduce src/local-fs-adaptor/*java .
--- build-command.sh
+++ build-command.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e # fail if any command fails
 mkdir -p classes
-javac -d classes src/adaptorlib/*java src/adaptorlib/tests/*java src/templateadaptor/*java
+javac -d classes src/adaptorlib/*java src/adaptorlib/tests/*java src/adaptortemplate/*java
 cp src/logging.properties classes/
 
 # TODO: Reintroduce src/local-fs-adaptor/*java .
