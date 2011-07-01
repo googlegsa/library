e6f9ed3204977699c9d83a25dc488518a5030681
Eric Anderson <ejona@google.com>
2011-07-01 12:33:00 -0700

    Make Command class iterruptable

--- src/adaptorlib/Command.java
+++ src/adaptorlib/Command.java
@@ -6,8 +6,6 @@ import java.io.*;
  * Exec helper that allows easy handling of stdin, stdout, and stderr. Normally
  * you have to worry about deadlock when dealing with those streams, so this
  * class handles that for you.
- *
- * @author ejona@google.com (Eric Anderson)
  */
 public class Command {
   private int returnCode;
@@ -16,15 +14,18 @@ public class Command {
 
   public Command() {}
 
-  public int exec(String[] command) throws IOException {
+  public int exec(String[] command) throws IOException,
+         InterruptedException {
     return exec(command, null, new byte[0]);
   }
 
-  public int exec(String[] command, File workingDir) throws IOException {
+  public int exec(String[] command, File workingDir) throws IOException,
+         InterruptedException {
     return exec(command, workingDir, new byte[0]);
   }
 
-  public int exec(String[] command, byte[] stdin) throws IOException {
+  public int exec(String[] command, byte[] stdin) throws IOException,
+         InterruptedException {
     return exec(command, null, stdin);
   }
 
@@ -32,73 +33,90 @@ public class Command {
    * @throws IOException if creating process fails
    */
   public int exec(String[] command, File workingDir, byte[] stdin)
-      throws IOException {
+      throws IOException, InterruptedException {
+    // Clear so that if the object is reused, and the second use has an
+    // InterruptedException, they don't accidentally use the wrong data.
+    stdout = null;
+    stderr = null;
+
     Process proc = Runtime.getRuntime().exec(command, null, workingDir);
     Thread in, out, err;
     in = new Thread(new StreamCopyRunnable(new ByteArrayInputStream(stdin),
                                            proc.getOutputStream(), true));
+    in.setDaemon(true);
     in.start();
 
     ByteArrayOutputStream outBuffer = new ByteArrayOutputStream();
     out = new Thread(new StreamCopyRunnable(proc.getInputStream(), outBuffer,
                                             true));
+    out.setDaemon(true);
     out.start();
 
     ByteArrayOutputStream errBuffer = new ByteArrayOutputStream();
     err = new Thread(new StreamCopyRunnable(proc.getErrorStream(), errBuffer,
                                             true));
+    err.setDaemon(true);
     err.start();
 
-    silentJoin(in);
-    silentJoin(out);
-    silentJoin(err);
+    try {
+      in.join();
+      out.join();
+      err.join();
 
-    returnCode = silentWaitFor(proc);
+      returnCode = proc.waitFor();
+    } catch (InterruptedException ex) {
+      // Our threads should stop once the process closes.
+      // This destroy() is quite rude to the subprocess, but there is not any
+      // way to inform it to abort.
+      proc.destroy();
+      throw ex;
+    }
+
     stdout = outBuffer.toByteArray();
     stderr = errBuffer.toByteArray();
 
     return returnCode;
   }
 
-  private void silentJoin(Thread thread) {
-    while (true) {
-      try {
-        thread.join();
-      } catch (InterruptedException ex) {
-        continue;
-      }
-      break;
-    }
-  }
-
-  private int silentWaitFor(Process proc) {
-    int ret;
-    while (true) {
-      try {
-        ret = proc.waitFor();
-      } catch (InterruptedException ex) {
-        continue;
-      }
-      break;
-    }
-    return ret;
-  }
-
   public int getReturnCode() {
     return returnCode;
   }
 
+  /**
+   * Returns internal byte array without copying.
+   */
   public byte[] getStdout() {
     return stdout;
   }
 
+  /**
+   * Returns internal byte array without copying.
+   */
   public byte[] getStderr() {
     return stderr;
   }
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException,
+         InterruptedException {
     Command command = new Command();
-    command.exec(new String[] {"cat"}, "hello".getBytes());
+    if (true) {
+      command.exec(new String[] {"cat"}, "hello".getBytes());
+    } else {
+      final Thread mainThread = Thread.currentThread();
+      Thread interruptThread = new Thread() {
+        public void run() {
+          try {
+            Thread.sleep(1000);
+          } catch (InterruptedException ex) {
+            // ignore, but stop sleep
+          }
+          mainThread.interrupt();
+        }
+      };
+      interruptThread.setDaemon(true);
+      interruptThread.start();
+      command.exec(new String[] {"sleep", "1m"});
+    }
     System.out.println("" + command.getReturnCode() + "\n"
                        + new String(command.getStderr()) + "\n"
                        + new String(command.getStdout()) + "\n");
@@ -108,10 +126,6 @@ public class Command {
     private InputStream is;
     private OutputStream os;
     private boolean autoClose;
-
-    public StreamCopyRunnable(InputStream is, OutputStream os) {
-      this(is, os, false);
-    }
 
     public StreamCopyRunnable(InputStream is, OutputStream os,
                               boolean autoClose) {
@@ -128,7 +142,7 @@ public class Command {
           os.write(buffer, 0, read);
         }
       } catch (IOException ex) {
-        // ignore
+        // Ignore, but stop thread
       } finally {
         if (autoClose) {
           silentClose(is);
