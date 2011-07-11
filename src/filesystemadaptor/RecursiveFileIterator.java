d651192c3180e30e7f162c9861539224b428e6e7
Eric Anderson <ejona@google.com>
2011-07-11 10:06:28 -0700

    Improve readability of RecursiveFileIterator and add test

--- src/filesystemadaptor/RecursiveFileIterator.java
+++ src/filesystemadaptor/RecursiveFileIterator.java
@@ -2,18 +2,28 @@ package filesystemadaptor;
 
 import adaptorlib.*;
 import java.io.File;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+/**
+ * Iterate over all files within a folder, including files in subdirectories.
+ */
 class RecursiveFileIterator implements Iterator<File>, Iterable<File> {
-  private List<List<File>> traversalStateStack
-      = new ArrayList<List<File>>();
+  /**
+   * The recursive call stack of directory content we have encountered. As we
+   * recursively descend into directories, we add one item in the outer list for
+   * each directory. The contents of the inner List are the files and folders
+   * within that directory. The stack is pushed and poped from the front, so the
+   * top-most directory contents are at the end. Things are removed from the
+   * inner list as they are consumed by next().
+   */
+  private List<List<File>> traversalStateStack = new LinkedList<List<File>>();
 
   public RecursiveFileIterator(File rootFile) {
-    ArrayList<File> roots = new ArrayList<File>();
+    List<File> roots = new LinkedList<File>();
     roots.add(rootFile);
     traversalStateStack.add(roots);
   }
@@ -22,30 +32,29 @@ class RecursiveFileIterator implements Iterator<File>, Iterable<File> {
     return this;
   }
 
+  /**
+   * Make {@code traversalStateStack.get(0).get(0)} be the file that would be
+   * returned by {@link #next}, or have tranversalStateStack be empty. If things
+   * are already in the right place, then no action is performed.
+   */
   private void setPositionToNextFile() {
     while (traversalStateStack.size() > 0) {
-      int last = traversalStateStack.size() - 1;
-      List<File> l = traversalStateStack.get(last);
+      List<File> l = traversalStateStack.get(0);
 
       if (l.isEmpty()) {
-        traversalStateStack.remove(traversalStateStack.size() - 1);
+        traversalStateStack.remove(0);
       } else {
         File f = l.get(0);
         if (f.isDirectory()) {
           l.remove(0);
           List<File> child = Arrays.asList(f.listFiles());
-          traversalStateStack.add(new ArrayList<File>(child));
-        } else if (!isQualifiyingFile(f)) {
-          l.remove(0);
+          traversalStateStack.add(0, new LinkedList<File>(child));
         } else {
+          // Everything looks good
           return;
         }
       }
     }
-  }
-
-  private boolean isQualifiyingFile(File f) {
-    return true;
   }
 
   public boolean hasNext() {
@@ -57,21 +66,11 @@ class RecursiveFileIterator implements Iterator<File>, Iterable<File> {
     if (!hasNext()) {
       throw new NoSuchElementException();
     }
-    File next = traversalStateStack.get(
-        traversalStateStack.size() - 1).remove(0);
+    File next = traversalStateStack.get(0).remove(0);
     return next;
   }
 
   public void remove() {
     throw new UnsupportedOperationException();
-  }
-
-  public static void main(String a[]) {
-    File d = new File("/usr/local/google/home/pjo/smbdup/800k/");
-    Iterator<File> itr = new RecursiveFileIterator(d);
-    for (int count = 0; itr.hasNext(); count++) {
-      File f = itr.next();
-      System.out.println("" + f);
-    }
   }
 }
---
+++ test/filesystemadaptor/RecursiveFileIteratorTest.java
@@ -0,0 +1,69 @@
+package filesystemadaptor;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.util.Iterator;
+
+public class RecursiveFileIteratorTest {
+  @Test
+  public void testIteration() {
+    File dir = new MockFile("parent", new File[] {
+      new MockFile("file2"),
+      new MockFile("dir1", new File[] {
+        new MockFile("dir2", new File[0]),
+        new MockFile("file3"),
+      }),
+      new MockFile("file1"),
+      new MockFile("dir3", new File[] {
+        new MockFile("file4"),
+      }),
+    });
+    Iterator<File> iter = new RecursiveFileIterator(dir);
+    assertTrue(iter.hasNext());
+    assertEquals("file2", iter.next().getName());
+    assertTrue(iter.hasNext());
+    assertEquals("file3", iter.next().getName());
+    assertEquals("file1", iter.next().getName());
+    assertEquals("file4", iter.next().getName());
+    assertFalse(iter.hasNext());
+  }
+
+  private static class MockFile extends File {
+    private boolean isDirectory;
+    private String name;
+    private File[] children;
+
+    /** Constructor for mock files */
+    public MockFile(String name) {
+      super("trash");
+      isDirectory = false;
+      this.name = name;
+    }
+
+    /** Constructor for mock directories */
+    public MockFile(String name, File[] children) {
+      super("trash");
+      isDirectory = true;
+      this.name = name;
+      this.children = children;
+    }
+
+    @Override
+    public boolean isDirectory() {
+      return isDirectory;
+    }
+
+    @Override
+    public File[] listFiles() {
+      return children;
+    }
+
+    @Override
+    public String getName() {
+      return name;
+    }
+  }
+}
