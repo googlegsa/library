dc215865665e0e9d0214f83234c7d95bf4ef430e
Eric Anderson <ejona@google.com>
2011-11-04 11:22:11 -0700

    Decrease XSS exposure by eleminating innerHTML use
    The original reason for the innerHTML (via the html() method in jQuery)
    usage was to fix an IE rendering problem with text(). This is an
    alternative and superior fix.

--- resources/adaptorlib/static/dashboard.js
+++ resources/adaptorlib/static/dashboard.js
@@ -212,9 +212,9 @@ function getLogCallback(result, error) {
   if (result === null) {
     throw error;
   } else {
-    result = result.replace(/&/g, '&amp;').replace(/</g, '&lt;');
-    // Replace \n with <br> as a workaround for IE
-    $('#gaf-log').html(result.replace(/\n/g, '<br>'));
+    var gafLog = $('#gaf-log');
+    gafLog.empty();
+    gafLog.append(document.createTextNode(result));
   }
 }
 
