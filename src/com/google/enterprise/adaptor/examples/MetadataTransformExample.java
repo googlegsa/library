dc1d79127d71d866e293caba6136b344175d22a5
PJ <pjo@google.com>
2013-09-03 17:35:19 -0700

    provide example source of metadata transform

---
+++ src/com/google/enterprise/adaptor/examples/MetadataTransformExample.java
@@ -0,0 +1,79 @@
+// Copyright 2013 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.adaptor.examples;
+
+import com.google.enterprise.adaptor.DocumentTransform;
+import com.google.enterprise.adaptor.Metadata;
+
+import java.util.*;
+import java.util.logging.*;
+
+/**
+ * Example transform which will move values from one key to another.
+ * <p>
+ * For example the values under key "creator" could be moved to 
+ * be under the key "author".
+ */
+public class MetadataTransformExample implements DocumentTransform {
+  private static final Logger log
+      = Logger.getLogger(MetadataTransformExample.class.getName());
+  private final String src;
+  private final String dest;
+ 
+  private MetadataTransformExample(String originalKey, String changedKey) {
+    if (null == originalKey || null == changedKey) {
+      throw new NullPointerException();
+    }
+    this.src = originalKey;
+    this.dest = changedKey;
+    if (src.equals(dest)) {
+      log.log(Level.WARNING, "original and destination key the same: {0}", src);
+    }
+  }
+
+  /** Makes transform from config with "src" and "dest" keys. */
+  public static MetadataTransformExample create(Map<String, String> cfg) {
+    return new MetadataTransformExample(cfg.get("src"), cfg.get("dest"));
+  }
+
+  @Override
+  public void transform(Metadata metadata, Map<String, String> params) {
+    if (src.equals(dest)) {
+      return;
+    }
+    Set<String> valuesToMove = metadata.getAllValues(src);
+    if (valuesToMove.isEmpty()) {
+      log.log(Level.FINE, "no values for {0}. Skipping", src);
+    } else {
+      log.log(Level.FINE, "moving values from {0} to {1}: {2}",
+          new Object[] {src, dest, valuesToMove});
+      Set<String> valuesAlreadyThere = metadata.getAllValues(dest);
+      metadata.set(dest, combine(valuesToMove, valuesAlreadyThere));
+      log.log(Level.FINER, "deleting source {0}", src);
+      metadata.set(src, Collections.<String>emptySet());
+    }
+  }
+
+  private Set<String> combine(Set<String> s1, Set<String> s2) {
+    Set<String> combined = new HashSet<String>(s1);
+    combined.addAll(s2);
+    return combined;
+  }
+
+  @Override
+  public String toString() {
+    return "MetadataTransformExample(src=" + src + ",dest=" + dest + ")";
+  }
+}
---
+++ test/com/google/enterprise/adaptor/examples/MetadataTransformExampleTest.java
@@ -0,0 +1,78 @@
+// Copyright 2013 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.adaptor.examples;
+
+import static org.junit.Assert.*;
+
+import com.google.enterprise.adaptor.DocumentTransform;
+import com.google.enterprise.adaptor.Metadata;
+
+import org.junit.*;
+import org.junit.rules.ExpectedException;
+
+import java.util.*;
+
+/** Unit tests for {@link MetadataTransformExample}. */
+public class MetadataTransformExampleTest {
+  @Rule
+  public ExpectedException thrown = ExpectedException.none();
+
+  private static Map<String, String> makeConfig(String src, String dest) {
+    Map<String, String> config = new HashMap<String, String>();
+    config.put("src", src);
+    config.put("dest", dest);
+    return config;
+  }
+
+  @Test
+  public void testMove() {
+    final Metadata metadataGolden;
+    {
+      Metadata golden = new Metadata();
+      golden.add("color", "orange");
+      golden.add("color", "keylime");
+      golden.add("color", "oxblood");
+      metadataGolden = golden.unmodifiableView();
+    }
+    Metadata metadata = new Metadata();
+    metadata.add("colour", "keylime");
+    metadata.add("color", "oxblood");
+    metadata.add("colour", "orange");
+    DocumentTransform transform 
+        = MetadataTransformExample.create(makeConfig("colour", "color"));
+    transform.transform(metadata, new HashMap<String, String>());
+    assertEquals(metadataGolden, metadata);
+  }
+
+  @Test
+  public void testDegenerateMove() {
+    final Metadata metadataGolden;
+    {
+      Metadata golden = new Metadata();
+      golden.add("color", "orange");
+      golden.add("color", "keylime");
+      golden.add("color", "oxblood");
+      metadataGolden = golden.unmodifiableView();
+    }
+    Metadata metadata = new Metadata();
+    metadata.add("color", "keylime");
+    metadata.add("color", "oxblood");
+    metadata.add("color", "orange");
+    DocumentTransform transform 
+        = MetadataTransformExample.create(makeConfig("color", "color"));
+    transform.transform(metadata, new HashMap<String, String>());
+    assertEquals(metadataGolden, metadata);
+  }
+}
