fec5507ecc2d2b37999a313787f189ba120a227b
Dominik Weidenfeld <dominik.weidenfeld@twt.de>
2015-08-18 08:39:41 +0200

    changed some wording and exception naming

--- src/com/google/enterprise/adaptor/ContentTransformFactory.java
+++ src/com/google/enterprise/adaptor/ContentTransformFactory.java
@@ -7,6 +7,7 @@ import java.lang.reflect.Constructor;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * The content transform factory holds all document content transforms
@@ -24,22 +25,22 @@ public class ContentTransformFactory {
    * configuration values for the {@link DocumentContentTransform}
    * instantiation.
    *
-   * @param transforms Configuration for each {@link DocumentContentTransform}
+   * @param configs Configuration for each {@link DocumentContentTransform}
    * @throws InvalidConfigurationException If the class for a {@link
    *                                       DocumentContentTransform} is missing
    * @throws RuntimeException              If the class does not match all
    *                                       criteria
    */
   public ContentTransformFactory(
-      final List<Map<String, String>> transforms) {
-    if (transforms.size() <= 0) {
+      final List<Map<String, String>> configs) {
+    if (configs.size() <= 0) {
       return;
     }
     this.transforms = new LinkedHashMap<Constructor<DocumentContentTransform>,
         Map<String, String>>();
-    for (int i = (transforms.size() - 1); i >= 0; i--) {
-      final Map<String, String> tConfig = transforms.get(i);
-      final String className = tConfig.get("class");
+    for (int i = (configs.size() - 1); i >= 0; i--) {
+      final Map<String, String> config = configs.get(i);
+      final String className = config.get("class");
       if (Strings.isNullOrEmpty(className)) {
         throw new InvalidConfigurationException(
             "Document Content Transform class is missing: " + className);
@@ -50,9 +51,9 @@ public class ContentTransformFactory {
             (Class<DocumentContentTransform>) Class.forName(className);
         final Constructor<DocumentContentTransform> constructor =
             clazz.getConstructor(Map.class);
-        this.transforms.put(constructor, tConfig);
+        this.transforms.put(constructor, new TreeMap<String, String>(config));
       } catch (Exception e) {
-        throw new RuntimeException(
+        throw new InvalidConfigurationException(
             "Cannot get document content transform of type: " + className, e);
       }
     }
