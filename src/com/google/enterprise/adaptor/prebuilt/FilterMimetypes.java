3e5d8cf7ded590ef0d2154b8c3d9e4ece1cfe100
PJ <git-pjo.google.com>
2015-09-11 14:48:31 -0700

    cache mimetype filter decisions

--- src/com/google/enterprise/adaptor/prebuilt/FilterMimetypes.java
+++ src/com/google/enterprise/adaptor/prebuilt/FilterMimetypes.java
@@ -22,6 +22,7 @@ import com.google.enterprise.adaptor.MetadataTransform;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
@@ -35,6 +36,15 @@ public class FilterMimetypes implements MetadataTransform {
   private final Set<String> supported;
   private final Set<String> unsupported;
   private final Set<String> excluded;
+  private final Map<String, String> decided;
+
+  private synchronized String lookupDecision(String ct) {
+    return decided.get(ct);
+  }
+
+  private synchronized void insertDecision(String ct, String d) {
+    decided.put(ct, d);
+  }
 
   private FilterMimetypes(Set<String> s, Set<String> u, Set<String> e) {
     if (null == s || null == u || null == e) {
@@ -43,6 +53,7 @@ public class FilterMimetypes implements MetadataTransform {
     supported = s;
     unsupported = u;
     excluded = e;
+    decided = new HashMap<String, String>();
   }
 
   public Set<String> getSupportedMimetypes() {
@@ -69,11 +80,19 @@ public class FilterMimetypes implements MetadataTransform {
       ct = ct.substring(0, semicolonIndex);
     }
     ct = ct.trim().toLowerCase();
+    String decision = lookupDecision(ct);
+    if (null != decision) {
+      params.put("Transmission-Decision", decision);
+      return;
+    }
     if (matches(supported, ct, "supported")) {
+      insertDecision(ct, "as-is");
       params.put("Transmission-Decision", "as-is");
     } else if (matches(unsupported, ct, "unsupported")) {
+      insertDecision(ct, "do-not-index-content");
       params.put("Transmission-Decision", "do-not-index-content");
     } else if (matches(excluded, ct, "excluded")) {
+      insertDecision(ct, "do-not-index");
       params.put("Transmission-Decision", "do-not-index");
     } else {
       log.info("unknown mime-type: " + ct);
