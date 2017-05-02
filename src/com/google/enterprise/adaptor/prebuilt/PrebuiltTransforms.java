6e56c892f03703e94bf5115f38cab5687ad1b0ca
Brett Johnson <Brett.Michael.Johnson@gmail.com>
2017-05-01 17:52:37 -0700

    Revert debugging logging

--- src/com/google/enterprise/adaptor/prebuilt/PrebuiltTransforms.java
+++ src/com/google/enterprise/adaptor/prebuilt/PrebuiltTransforms.java
@@ -562,15 +562,15 @@ public class PrebuiltTransforms {
     private void replaceInParams(String key, Map<String, String> params) {
       String original = getTrimmedValue(params, key);
       if (original == null) {
-        log.log(Level.INFO, "No param value for {0}. Skipping", key);
+        log.log(Level.FINE, "No param value for {0}. Skipping", key);
         return;
       }
-      log.log(Level.INFO,
+      log.log(Level.FINE,
           "Replacing param value of {0} that match {1} with {2}: {3}",
           new Object[] {key, toMatch, replacement, original});
       String newValue = toMatch.matcher(original).replaceAll(replacement);
       params.put(key, newValue);
-      log.log(Level.INFO, "After replacing param value for {0}: {1}",
+      log.log(Level.FINE, "After replacing param value for {0}: {1}",
           new Object [] {key, newValue});
     }
 
