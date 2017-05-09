9cb94f4ae326c54e51a7e0206a0df02559614934
Brett Johnson <Brett.Michael.Johnson@gmail.com>
2017-05-08 19:03:18 -0700

    JL feedback

--- src/com/google/enterprise/adaptor/MetadataTransform.java
+++ src/com/google/enterprise/adaptor/MetadataTransform.java
@@ -14,6 +14,8 @@
 
 package com.google.enterprise.adaptor;
 
+import static java.util.Locale.US;
+
 import java.util.Map;
 
 /**
@@ -25,34 +27,57 @@ import java.util.Map;
  */
 public interface MetadataTransform {
 
-  /** Keys for data stored in the {@code Map} of {@code params}. */
+  /** The key for the document's unique ID supplied in the {@code params}. */
   public static final String KEY_DOC_ID = "DocId";
+
+  /** The key for the document's display URL supplied in the {@code params}. */
   public static final String KEY_DISPLAY_URL = "Display-URL";
+
+  /** The key for the document's content type supplied in the {@code params}. */
   public static final String KEY_CONTENT_TYPE = "Content-Type";
+
+  /** The key for the document's crawl once flag supplied in the {@code params}.
+   */
   public static final String KEY_CRAWL_ONCE = "Crawl-Once";
+
+  /** The key for the document's lock flag supplied in the {@code params}. */
   public static final String KEY_LOCK = "Lock";
+
+  /** The key for the document's last modified timestamp supplied in the
+   * {@code params}.
+   */
   public static final String KEY_LAST_MODIFIED_MILLIS_UTC
       = "Last-Modified-Millis-UTC";
+
+  /** The key for a {@link TransmissionDecision} that may be added to the
+   * {@code params}.
+   */
   public static final String KEY_TRANSMISSION_DECISION 
       = "Transmission-Decision";
 
   /**
    * Identifies the location a configured key/value pair resides,
-   * either in the {@link Metadata}, or the map of {@code params}.
+   * either in the {@link Metadata}, or the map of {@code params}
+   * supplied to the {@link #transform} method of a {@code MetadataTransform}.
    * <p>
-   * Suppose a transform wishes examine or modify the display URL, which is
-   * stored in the {@code params}. The transform's configuration might look
-   * like:
+   * Suppose a transform wishes to examine or modify the display URL, which is
+   * stored in the {@code params}. The transform's configuration might include:
    * <pre><code>
      key=Display-URL
      keyset=params
      </code></pre>
-   * If unspecified, the default value is {@code metadata}.
+   * If unspecified, the default value is {@code METADATA}.
    *
    * @since 4.1.4
    */
   public enum Keyset {
+    /** The key/value(s) reside in the {@link Metadata}.
+     *  This value's name is {@code metadata}.
+     */
     METADATA("metadata"),
+    /** The key/value pair resides in the {@code Map} of {@code params}.
+     *  This value's name is {@code params}.
+     */
     PARAMS("params");
 
     private final String name;
@@ -61,20 +86,43 @@ public interface MetadataTransform {
       this.name = name;
     }
 
-    public static Keyset from(String val) {
-      return (val == null) ? METADATA : Keyset.valueOf(val.toUpperCase());
+    /**
+     * Returns a {@code Keyset} value with the given {@code name}, or the
+     * default value of {@code METADATA} if name is {@code null}.
+     * @param name the name of the Keyset value
+     * @throws IllegalArgumentException if there is no Keyset value with
+     *         {@code name}.
+     */
+    public static Keyset from(String name) {
+      return (name == null) ? METADATA : Keyset.valueOf(name.toUpperCase(US));
     }
 
+    /** Returns the name of this {@code Keyset} value. */
     @Override
     public String toString() {
       return name;
     }
   }
 
-  /** Transforms can cancel sending doc, or cancel sending its contents. */
+  /**
+   * Transforms can cancel sending a document, or cancel sending its contents by
+   * adding a {@code Transmission-Decision} to the {@code Map} of {@code params}
+   * supplied to the {@code transform} method of the {@code MetadataTransform}.
+   * If no {@code Transmission-Decision} is set in the params, the default value
+   * of {@code AS_IS} is used.
+   */
   public enum TransmissionDecision {
+    /** The document's metadata and content are to remain intact.
+     *  This value's name is {code as-is}.
+     */
     AS_IS("as-is"),
+    /** The document's metadata and content are discarded.
+     *  This value's name is {code do-not-index}.
+     */
     DO_NOT_INDEX("do-not-index"),
+    /** The document's content is discarded, but its metadata remains intact.
+     *  This value's name is {code do-not-index-content}.
+     */
     DO_NOT_INDEX_CONTENT("do-not-index-content");
 
     private final String name;
@@ -83,13 +131,22 @@ public interface MetadataTransform {
       name = n;
     }
 
-    public static TransmissionDecision from(String val) {
-      if (null == val) {
+    /**
+     * Returns a {@code TransmissionDecision} value with the given {@code name},
+     * or the default value of {@code AS_IS} if name is {@code null}.
+     * @param name the name of the TransmissionDecision value
+     * @throws IllegalArgumentException if there is no TransmissionDecision
+     *         value with {@code name}.
+     */
+    public static TransmissionDecision from(String name) {
+      if (null == name) {
         return AS_IS;
       }
-      return TransmissionDecision.valueOf(val.replace('-', '_').toUpperCase());
+      return
+          TransmissionDecision.valueOf(name.replace('-', '_').toUpperCase(US));
     }
 
+    /** Returns the name of this {@code TransmissionDecision} value. */
     @Override
     public String toString() {
       return name;
