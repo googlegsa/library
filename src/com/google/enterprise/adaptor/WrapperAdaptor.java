281f5aa126a2de751e6ec1eb2bc0e0ea342579df
Brett Johnson <BrettMichaelJohnson@users.noreply.github.com>
2017-06-12 22:21:05 -0700

    Fix unused imports after conflict resolution. (#58)

--- src/com/google/enterprise/adaptor/WrapperAdaptor.java
+++ src/com/google/enterprise/adaptor/WrapperAdaptor.java
@@ -14,8 +14,6 @@
 
 package com.google.enterprise.adaptor;
 
-import com.google.enterprise.adaptor.MetadataTransform.TransmissionDecision;
-
 import com.sun.net.httpserver.HttpContext;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
